6e6bee0460f67c727a2201b0ceb361e82125eaec
Yernur <shalgymbayev.yernur2001@gmail.com>
2025-02-26 16:09:08 +0500

    chore: add shadcn

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,3 +1,9 @@
+import { Button } from "@/shared/components/ui/button";
+
 export const App = () => {
-  return <div className="bg-black text-white min-h-screen">App</div>;
+  return (
+    <div className="bg-black text-white min-h-screen">
+      <Button>Click me</Button>
+    </div>
+  );
 };
