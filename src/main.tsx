131b2b13777756b2f0703110eae7cc04df74cd73
Yernur <shalgymbayev.yernur2001@gmail.com>
2025-02-27 18:57:31 +0500

    chore: add sentry

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,25 @@
 import "./main.css";
 
+import * as Sentry from "@sentry/react";
 import React from "react";
 import { createRoot } from "react-dom/client";
 
 import { App } from "./app/App";
+
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: [
+    "localhost",
+    import.meta.env.VITE_SENTRY_PROD_URL_RGX,
+  ],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
 
 const container = document.querySelector("#root");
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,13 @@ import { Button } from "@/shared/components/ui/button";
 export const Home = () => {
   return (
     <div className="min-h-screen grid place-items-center">
-      <Button>Click me</Button>
+      <Button
+        onClick={() => {
+          throw new Error("This is error");
+        }}
+      >
+        Click me
+      </Button>
     </div>
   );
 };
